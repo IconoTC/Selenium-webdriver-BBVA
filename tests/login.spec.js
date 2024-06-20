07e002659b4ee25406d0d37a15b845059caf6385
Javier Flores <javifb1983@gmail.com>
2024-06-20 17:47:28 +0200

    first login test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,17 +2,17 @@ const { Builder, By, until } = require('selenium-webdriver');
 const assert = require('assert');
 
 
-describe.skip('login', () => {
+describe('login', () => {
     
   it("visit the login url", async function() {
-      this.timeout(10000);
+     // this.timeout(10000);
       let driver = new Builder().forBrowser('chrome').build();
   
       try {
-          await driver.get("https://www.saucedemo.com/");
-          await driver.sleep(1000); // Puedes considerar eliminar esto si usas esperas explícitas
+          await driver.get("https://www.saucedemo.com/"); //Visitamos la url
+          await driver.sleep(1000); // Espera implicita
           const url = await driver.getCurrentUrl();
-          assert(url.includes('saucedemo.com'), 'URL should include saucedemo.com');
+          assert(url.includes('saucedemo.com'), 'URL should include saucedemo.com'); //Comprobamos que la url contiene saucedemo.com
           // Espera explícita hasta que el elemento esté localizado y visible
           await driver.wait(until.elementLocated(By.id('user-name')), 5000);
           const userNameField = await driver.findElement(By.id('user-name'));
