b8d47fbbb09f085e4a136a8113650b56c59b052c
Javier Flores <javifb1983@gmail.com>
2024-06-21 11:38:49 +0200

    should fail login when password is not provided

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -34,7 +34,7 @@ describe('login',  function () {
         assert.ok(divLog); 
      });
 
-  it.only('should fail login when username and password are not provided', async function () {
+  it('should fail login when username and password are not provided', async function () {
     let pageText = await driver.findElement(By.css('body')).getText();
     assert(!pageText.includes('Epic sadface: Username is required'), `El texto "'username is required'" no se encontró en la página`);
     await driver.findElement(By.css('[data-test="login-button"]')).click();
@@ -45,6 +45,33 @@ describe('login',  function () {
     assert(pageText.includes('Epic sadface: Username is required'), `El texto "'username is required'" no se encontró en la página`);    
   });
 
-  // it.only('should fail login when password is not provided and appears an error message "Epic sadface: Password is required"', async function () {
+  it('should fail login when password is not provided and appears an error message "Epic sadface: Password is required"', async function () {
+    this.timeout(20000);
+    let pageText = await driver.findElement(By.css('body')).getText();
+    const getInputUsername = await driver.findElement(By.id('user-name')) //Guardamos en una constante el localizador de un elemento web (El input Username)
+    let getInputValue = await getInputUsername.getAttribute('value') // Usamos getAttribute('value) para obtener el value de ese elemento web
+    console.log('value:', getInputValue);
+    assert(getInputValue.includes('')) //Verificamos que el value es una string vacía
+    assert.strictEqual(getInputValue, '');
 
+    getInputUsername.sendKeys('User'); // Obtener el texto visible del elemento
+   // const inputUsername = await driver.findElement(By.css('[data-test="username"]'))
+
+    getInputValue = await driver.findElement(By.id('user-name')).getAttribute('value')
+    console.log('value:', getInputValue);
+    assert(getInputValue.includes('User'))//Verificamos que el value contiene el texto 'User'
+    assert.strictEqual(getInputValue, 'User');//Verificamos que el value es exactamente 'User'
+
+
+    assert(!pageText.includes('Epic sadface: Username is required'), `El texto "'username is required'" se encontró en la página`);
+    //Hacemos click
+    await driver.findElement(By.css('[data-test="login-button"]')).click();
+    //Comprobar que existe el elemento web del error
+    const divLog = await driver.wait(until.elementLocated(By.css('[data-test="error-button"]')), 5000);
+    pageText = await driver.findElement(By.css('body')).getText();
+    await driver.sleep(5000);
+    assert.ok(divLog); 
+    assert(pageText.includes('Epic sadface: Password is required'), `El texto "'username is required'" no se encontró en la página`);
+
+  });
 });
